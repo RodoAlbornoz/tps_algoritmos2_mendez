73126cbcb48b15cb1d9d397d70a3a2af809a110d
rvalbor01 <ralbornozt@fi.uba.ar>
2022-06-09 11:52:49 -0300

    Se termina de implementar la funcion de creacion de hash junto a sus pruebas

--- tda_hash/pruebas.c
+++ tda_hash/pruebas.c
@@ -1,20 +1,102 @@
 #include "src/hash.h"
 #include "pa2m.h"
+#include "src/lista.h"
+
+struct hash {
+	lista_t **tabla;
+	size_t capacidad;
+	size_t almacenados;
+};
+
+void con_capacidad_menor_a_3_se_crea_el_hash_con_capacidad_3()
+{
+	struct hash *hash = hash_crear(1);
+
+	pa2m_afirmar(hash->capacidad == 3, 
+	"Se crea un hash con capacidad 3 al crearlo con capacidad menor a 3.");
+}
+
+
+void con_capacidad_mayor_o_igual_a_3_se_crea_el_hash_con_esa_capacidad()
+{
+	size_t capacidad = 7;
+	struct hash *hash = hash_crear(capacidad);
+
+	pa2m_afirmar(hash->capacidad == capacidad, 
+	"Se crea un hash con la capacidad indicada al ser esta 3 o mayor.");
+}
+
+
+void dado_un_hash_no_null_su_cantidad_empieza_en_0()
+{
+	struct hash *hash = hash_crear(4);
+	pa2m_afirmar(hash_cantidad(hash) == 0, 
+	"La cantidad de elementos almacenados del hash empieza en 0.");
+}
+
+
+/*
+ * Se realizan las pruebas sobre la funcion hash_crear
+ */
+void hash_probar_creacion()
+{
+	pa2m_nuevo_grupo("Pruebas de creacion de un hash");
+
+	con_capacidad_menor_a_3_se_crea_el_hash_con_capacidad_3();
+	con_capacidad_mayor_o_igual_a_3_se_crea_el_hash_con_esa_capacidad();
+	dado_un_hash_no_null_su_cantidad_empieza_en_0();
+}
+
+/*
+void dado_un_hash_null_se_retorna_cantidad_0()
+{
+	pa2m_afirmar(true, "Se retorna una cantidad de 0 para un hash NULL.");
+}
+
+
+void dado_un_hash_no_null_se_retorna_su_cantidad_de_elementos_almacenados()
+{
+	pa2m_afirmar(true, "Se retorna la cantidad de elementos almacenados dentro del hash para un hash no NULL.");
+}
 
 
 void hash_probar_cantidad()
 {
 	pa2m_nuevo_grupo("Pruebas de elementos almacenados en un hash");
 
-	hash_t *hash = hash_crear();
-
-	pa2m_afirmar(hash_cantidad(hash));
+	dado_un_hash_null_se_retorna_cantidad_0();
+	dado_un_hash_no_null_se_retorna_su_cantidad_de_elementos_almacenados();
 }
 
 
+void dado_un_hash_null_se_libera()
+{
+	pa2m_afirmar(true, "Se libera un hash NULL.");
+}
+
+
+void dado_un_hash_no_null_se_libera()
+{
+	pa2m_afirmar(true, "Se libera un hash no NULL.");
+}
+
+
+void hash_probar_destruir()
+{
+	pa2m_nuevo_grupo("Pruebas de destruccion de hash.");
+
+	dado_un_hash_null_se_libera();
+	dado_un_hash_no_null_se_libera();
+}
+*/
+
 int main()
 {
+	hash_probar_creacion();
+	/*
 	hash_probar_cantidad();
 
+	hash_probar_destruir();
+	*/
 	return pa2m_mostrar_reporte();
 }
--- tda_hash/src/hash.c
+++ tda_hash/src/hash.c
@@ -1,84 +1,95 @@
 #include "hash.h"
 #include "lista.h"
-
-
-
-typedef struct nodo {
-	void *elemento;
-	struct nodo *siguiente;
-} nodo_t;
-
-typedef struct lista {
-	nodo_t *nodo_inicio;
-	nodo_t *nodo_fin;
-	size_t cantidad;
-} lista_t;
-
+#include <stdlib.h>
+#include <stdbool.h>
 
 typedef struct clave_valor {
 	char *clave;
 	void *valor;
-	int borrado; // ESTA BIEN?
 } clave_valor_t;
 
 
-typedef struct hash {
+struct hash {
 	lista_t **tabla;
-	int capacidad;
-	int almacenados;
-} hash_t;
-
-
-float factor_de_carga(hash_t hash)
-{
-	return hash.capacidad / hash.almacenados;
-}
+	size_t capacidad;
+	size_t almacenados;
+};
 
 
 hash_t *hash_crear(size_t capacidad)
 {
-	hash_t *hash = malloc(sizeof(hash_t));
-
+	struct hash *hash = malloc(sizeof(struct hash));
 	if (hash == NULL)
 		return NULL;
 
-	// INICIALIZAR TABLA
+	hash->capacidad = capacidad < 3? 3 : capacidad;
 
-	if (capacidad < 3)
-		hash->capacidad = 3;
-	else 
-		hash->capacidad = capacidad;
+	hash->tabla = malloc(hash->capacidad * sizeof(lista_t *));
+	if (hash->tabla == NULL) {
+		free(hash);
+		return NULL;
+	}
+
+	for (int i = 0; i < hash->capacidad; i++)
+		hash->tabla[i] = NULL;
 
 	hash->almacenados = 0;
 
 	return hash;
 }
 
+/*
+hash_t *rehashear()
+{
+	return hash;
+}
+*/
 
-hash_t *hash_crear(size_t capacidad);
-
-
+/*
+ * Si la clave ya existía y se reemplaza el elemento, se almacena un puntero al
+ * elemento reemplazado en *anterior, si anterior no es NULL.
+ *
+ * Si la clave no existía y anterior no es NULL, se almacena NULL en *anterior.
+ *
+ * La función almacena una copia de la clave provista por el usuario,
+ *//*
 hash_t *hash_insertar(hash_t *hash, const char *clave, void *elemento,
 		      void **anterior)
 {	
-	return hash;
-}
+	float factor_de_carga = hash->almacenados / hash->capacidad;
 
+	if (factor_de_carga >= LIMITE_FACTOR_DE_CARGA)
+		hash = rehashear();
+
+	int posicion_tabla = funcion_hash(clave);
+
+	hash->tabla[posicion_tabla]->elemento = elemento;
+
+	return hash;
+}*/
+
+/*
 void *hash_quitar(hash_t *hash, const char *clave)
 {
-	return NULL;
+	if (hash == NULL)
+		return NULL;
 }
+
 
 void *hash_obtener(hash_t *hash, const char *clave)
 {
-	return NULL;
+	if (hash == NULL)
+		return NULL;
 }
+
 
 bool hash_contiene(hash_t *hash, const char *clave)
 {
-	return false;
+	if (hash == NULL)
+		return false;
 }
 
+*/
 size_t hash_cantidad(hash_t *hash)
 {
 	if (hash == NULL)
@@ -87,13 +98,26 @@ size_t hash_cantidad(hash_t *hash)
 	return hash->almacenados;
 }
 
+
 void hash_destruir(hash_t *hash)
 {
+	hash_destruir_todo(hash, NULL);
 }
+
 
 void hash_destruir_todo(hash_t *hash, void (*destructor)(void *))
 {
+	if (hash == NULL)
+		return;
+
+	for (int i = 0; i < hash_cantidad(hash); i++)
+		for (int j = 0; j < hash->tabla[i]->cantidad; j++)
+			lista_destruir_todo(hash->tabla[i], destructor);
+
+	free(hash->tabla);
+	free(hash);
 }
+
 
 size_t hash_con_cada_clave(hash_t *hash,
 			   bool (*f)(const char *clave, void *valor, void *aux),
