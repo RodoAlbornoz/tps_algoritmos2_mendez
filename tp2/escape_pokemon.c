c8836c555cad5ae0c29133be656e205600277ac1
rvalbor01 <ralbornozt@fi.uba.ar>
2022-06-23 16:12:02 -0300

    Se implementa la interfaz básica del juego del tp2

--- tp2/escape_pokemon.c
+++ tp2/escape_pokemon.c
@@ -1,18 +1,164 @@
 #include "src/sala.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+void imprimir_pantalla_de_inicio()
+{
+	printf("¡Bienvenido al juego de Escape Pokemon jugador! :D\n");
+	printf("Te despertas en una sala desconocida y no sabes qué hacer.\n");
+	printf("Tu objetivo es escapar de esta sala interactuando con los \n\
+		objetos de la misma\n");
+	printf("Empezas con un primer objeto y a partir de él tenes que \n\
+		buscar la forma de abrir la puerta.\n");
+}
+
+
+void imprimir_comandos()
+{
+	printf("1. Ayuda.\n");
+	printf("2. Agarrar <objeto>.\n");
+	printf("3. Describir <objeto>.\n");
+	printf("4. Información conocida (Imprime la información conocida de la sala).");
+	printf("5. Salir.\n");
+	system("clear");
+}
+
+
+void imprimir_ayuda()
+{
+	printf("INTERACCIONES \n"); 
+	printf("Acciones posibles:\n"); // Imprimir segundo campo de archivo de interacciones
+	printf("Para interacciones de 1 objeto, deben tener el formato: \n"); // Formato de interacciones
+	printf("Para interacciones de 2 objetos, las interacciones deben tener el formato: \n"); // Formato de interacciones
+}
+
+
+void imprimir_informacion_conocida()
+{
+	printf("\n");
+	printf("\n");
+	printf("\n");
+}
+
+
+void verificar_accion()
+{
+	// if (2 objetos y no se posee el primero)
+	// printf("Accion inválida.");
+	// Mostrar accion mensaje luego de completada
+	switch (cuarto campo de archivo de interacciones) {
+		case 'm':
+			//mostrar mensaje
+		break;
+
+		case 'e':
+			// eliminar objeto
+		break;
+
+		case 'd':
+			// descubrir objeto
+		break;
+
+		case 'g':
+			// descubrir objeto
+		break;
+
+		case 'r':
+			// reemplazar objeto
+		break;
+	}
+}
+
+
+void agarrar_objeto(sala_t *sala)
+{
+
+}
+
+
+void describir_objeto(sala_t *sala)
+{
+
+}
+
+
+void evaluar_comando(bool *salir, sala_t *sala)
+{
+	char *comando;
+	char *accion;
+	char *aux;
+	scanf("Ingrese comando: %s", comando);
+
+	sscanf(comando, "%s %s", accion, aux);
+
+	if (strcmp(accion, "ayuda") == 0 || strcmp(accion, "Ayuda") == 0) {
+		imprimir_ayuda();
+
+	} else if (strcmp(accion, "agarrar") == 0 || 
+		   strcmp(accion, "Agarrar") == 0) {
+		agarrar_objeto(sala);
+
+	} else if (strcmp(accion, "describir") == 0 || 
+		   strcmp(accion, "Describir") == 0) {
+		describir_objeto(sala);
+
+	} else if ((strcmp(accion, "Informacion") == 0 || 
+		    strcmp(accion, "informacion") == 0) &&
+		   (strcmp(aux, "conocida") == 0 ||
+		    strcmp(aux, "Conocida") == 0)) {
+		imprimir_informacion_conocida();
+
+	} else if (/*Es algun comando del archivo de interacciones*/) {
+		verificar_accion();
+
+	} else if (strcmp(accion, "salir") == 0 || 
+		   strcmp(accion, "Salir") == 0){
+		*salir = true; 
+
+	} else {
+		printf("El comando que insertaste NO es correcto.\n");
+		printf("Por favor, ingresa un comando valido o escribí \"ayuda\" para conocer los comandos posibles.\n");
+	}
+}
+
+
+bool argumentos_invalidos(int argc)
+{
+	if (argc != 3) {
+		printf("Cantidad de argumentos incorrecta.\n");
+		printf("Tip: Ingresá \"./ejecutable <archivo_objetos> <archivo_interacciones>\"\n");
+		return true;
+	}
+
+	return false;
+}
+
 
 int main(int argc, char *argv[])
 {
-	//Los archivos deben venir como parámetros del main
-	sala_t *sala = sala_crear_desde_archivos("???", "???");
+	if (argumentos_invalidos(argc))
+		return -1;
 
+	sala_t *sala = sala_crear_desde_archivos("objetos.txt", "interacciones.txt");
 	if (sala == NULL) {
-		printf("Error al crear la sala de escape\n");
+		printf("Error al crear la sala de escape.\n");
 		return -1;
 	}
 
+	imprimir_pantalla_de_inicio();
+	bool salir = false;
+	while (!sala_escape_exitoso(sala) && !salir) {
+		imprimir_comandos();
+		evaluar_comando(&salir, sala);
 
+		if (salir) {
+			printf("Saliendo de la sala. ¡Gracias por jugar! :D");
+			return -1;
+		}	
+	}
+
+	// Destruir vectores y otras cosas
 	sala_destruir(sala);
 
 	return 0;
--- tp2/src/sala.c
+++ tp2/src/sala.c
@@ -1,23 +1,185 @@
 #include "estructuras.h"
 #include "sala.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+// SON CORRECTOS ACA?
+#include "interaccion.h"
+#include "objeto.h"
+
+#define LARGO_MAX_LINEA 1024
+
+struct sala {
+	struct objeto **objetos;
+	int cantidad_objetos;
+	struct interaccion **interacciones;
+	int cantidad_interacciones;
+};
+
+/*
+ * PRE: Se recibe el path al archivo de texto objetos, la referencia a objetos->sala, y la referencia a la cantidad de objetos
+ * POST: Se devuelve un status sobre cómo se proceso la lectura del archivo (-1 es porque algo falló)
+ */
+int lectura_archivo_objetos(const char *objetos, struct objeto ***ref_vector_objetos, int* cantidad_objetos)
+{
+	FILE *archivo_objetos = fopen(objetos, "r");
+	if(!archivo_objetos)
+		return -1;
+
+	char linea_objetos[LARGO_MAX_LINEA];
+	char *leida_objetos = fgets(linea_objetos, LARGO_MAX_LINEA, archivo_objetos);
+
+	while (leida_objetos) {
+		struct objeto **bloque_objeto = realloc(*ref_vector_objetos, (unsigned) ((*cantidad_objetos)+1) * sizeof(struct objeto));
+		if (bloque_objeto == NULL) {
+			fclose(archivo_objetos);
+			return -1;
+		}
+
+		struct objeto *objeto_sala = objeto_crear_desde_string(linea_objetos);
+		if (objeto_sala == NULL) {
+			free(bloque_objeto);
+			fclose(archivo_objetos);
+			return -1;
+		}
+
+		bloque_objeto[*cantidad_objetos] = objeto_sala;
+		(*cantidad_objetos)++;
+		*ref_vector_objetos = bloque_objeto;
+
+		leida_objetos = fgets(linea_objetos, LARGO_MAX_LINEA, archivo_objetos);
+	}
+
+	fclose(archivo_objetos);
+	return 0;
+}
+
+
+/*
+ * PRE: Se recibe el path al archivo de texto interacciones, la referencia a objetos->interacciones, y la referencia a la cantidad de interacciones
+ * POST: Se devuelve un status sobre cómo se proceso la lectura del archivo (-1 es porque algo falló)
+ */
+int lectura_archivo_interacciones(const char *interacciones, struct interaccion ***ref_vector_interacciones, int* cantidad_interacciones)
+{
+	FILE *archivo_interacciones = fopen(interacciones, "r");
+	if(!archivo_interacciones)
+		return -1;
+
+	char linea_interacciones[LARGO_MAX_LINEA];
+	char *leida_interacciones = fgets(linea_interacciones, LARGO_MAX_LINEA, archivo_interacciones);
+
+	while (leida_interacciones) {
+		struct interaccion **bloque_interaccion = realloc(*ref_vector_interacciones, (unsigned) ((*cantidad_interacciones)+1) * sizeof(struct interaccion));
+		if (bloque_interaccion == NULL) {
+			fclose(archivo_interacciones);
+			return -1;
+		}
+
+		struct interaccion *interaccion_sala = interaccion_crear_desde_string(linea_interacciones);
+		if (interaccion_sala == NULL) {
+			free(bloque_interaccion);
+			fclose(archivo_interacciones);
+			return -1;
+		}
+
+		bloque_interaccion[*cantidad_interacciones] = interaccion_sala;
+		(*cantidad_interacciones)++;
+		*ref_vector_interacciones = bloque_interaccion;
+
+		leida_interacciones = fgets(linea_interacciones, LARGO_MAX_LINEA, archivo_interacciones);
+	}
+
+	fclose(archivo_interacciones);
+	return 0;
+}
+
 
 sala_t *sala_crear_desde_archivos(const char *objetos, const char *interacciones)
 {
-	return NULL;
+	sala_t *sala = malloc(sizeof(struct sala));
+	if (sala == NULL)
+		return NULL;
+
+	sala->cantidad_objetos = 0;
+	sala->cantidad_interacciones = 0;
+
+	sala->objetos = malloc(sizeof(struct objeto*));
+	sala->interacciones = malloc(sizeof(struct interaccion*));
+        if (sala->objetos == NULL || sala->interacciones == NULL) {
+		sala_destruir(sala);
+                return NULL;
+	}
+
+	if ((lectura_archivo_objetos(objetos, &(sala->objetos), &(sala->cantidad_objetos)) == -1) ||
+	   (lectura_archivo_interacciones(interacciones, &(sala->interacciones), &(sala->cantidad_interacciones)) == -1) ||
+	   (sala->cantidad_objetos == 0) || (sala->cantidad_interacciones == 0)) {
+		sala_destruir(sala);
+		return NULL;		   
+	}
+
+	return sala;
 }
+
 
 char **sala_obtener_nombre_objetos(sala_t *sala, int *cantidad)
 {
-	return NULL;
+	if (sala == NULL) {
+		if(cantidad != NULL)
+			*cantidad = -1;
+		return NULL;
+	}
+
+	char **vector_nombres = malloc((unsigned) sala->cantidad_objetos * sizeof(char *));
+	if (vector_nombres == NULL) {
+		free(sala);
+		if (cantidad != NULL)
+			*cantidad = -1; 
+		return NULL;
+	}
+
+	for (int i = 0; i < sala->cantidad_objetos; i++)
+		vector_nombres[i] = sala->objetos[i]->nombre;
+
+	if (cantidad != NULL)
+		*cantidad = sala->cantidad_objetos;
+
+	return vector_nombres;
 }
+
 
 bool sala_es_interaccion_valida(sala_t *sala, const char *verbo, const char *objeto1,
 				const char *objeto2)
 {
+	if (sala == NULL || verbo == NULL || objeto1 == NULL || objeto2 == NULL)
+		return false;
+
+	for (int i = 0; i < sala->cantidad_interacciones; i++) {
+		if ((strcmp(objeto1, sala->interacciones[i]->objeto) == 0) && 
+		   (strcmp(objeto2, sala->interacciones[i]->objeto_parametro) == 0) && 
+		   (strcmp(verbo, sala->interacciones[i]->verbo) == 0))
+			return true;
+	}
 	return false;
 }
 
+
 void sala_destruir(sala_t *sala)
 {
+	if (sala == NULL)
+		return;
+
+	for (int i = 0; i < sala->cantidad_objetos; i++)
+		free(sala->objetos[i]); 
+
+	for (int j = 0; j < sala->cantidad_interacciones; j++)
+		free(sala->interacciones[j]);
+
+	if (sala->objetos != NULL)
+		free(sala->objetos);
+
+	if (sala->interacciones != NULL)
+		free(sala->interacciones);
+
+	free(sala);
 }
