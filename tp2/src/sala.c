35cdb7a1728f45a4bdea329c57bb949de7130ab1
rvalbor01 <ralbornozt@fi.uba.ar>
2022-06-26 15:02:51 -0300

    Se implementa la funcion de interaccion valida y se resuelven errores de memoria

--- tp2/src/sala.c
+++ tp2/src/sala.c
@@ -15,6 +15,7 @@
 struct sala {
 	hash_t *objetos;
 	hash_t *objetos_conocidos;
+	hash_t *objetos_poseidos;
 	lista_t *interacciones;
 	bool escapado;
 };
@@ -23,6 +24,37 @@ struct info_objeto {
 	char descripcion[MAX_TEXTO];
 	bool es_asible;
 };
+
+struct nombres {
+	char **vector_nombres;
+	size_t cantidad_nombres;
+};
+
+struct elementos_interaccion {
+	const char *verbo;
+	const char *objeto1;
+	const char *objeto2;
+	bool es_valida;
+};
+
+
+/*
+ * Se recibe un string
+ *
+ * Se retorna una copia del mismo
+ */
+char *duplicar_string(const char *s)
+{
+	if (!s)
+		return NULL;
+
+	char *p = malloc(strlen(s) + 1);
+	if (!p)
+		return NULL;
+
+	strcpy(p, s);
+	return p;
+}
 
 
 /*
@@ -60,8 +92,12 @@ int lectura_archivo_objetos(const char *objetos, hash_t *hash_objetos)
 		strcpy(info_objeto->descripcion, objeto_sala->descripcion);
 		info_objeto->es_asible = objeto_sala->es_asible;
 
-		hash_objetos = hash_insertar(hash_objetos, 
-				objeto_sala->nombre, info_objeto, NULL);
+		char *copia_nombre = duplicar_string(objeto_sala->nombre);
+		hash_objetos = hash_insertar(hash_objetos, copia_nombre, 
+					     info_objeto, NULL);
+		free(info_objeto);
+		free(copia_nombre);
+		free(objeto_sala);
 
 		leida_objetos = fgets(linea_objetos, LARGO_MAX_LINEA, 
 				      archivo_objetos);
@@ -126,9 +162,11 @@ sala_t *inicializar_sala()
 	sala->escapado = false;
 	sala->objetos = hash_crear(CAPACIDAD_INICIAL_OBJETOS);
 	sala->objetos_conocidos = hash_crear(CAPACIDAD_INICIAL_OBJETOS);
+	sala->objetos_poseidos = hash_crear(CAPACIDAD_INICIAL_OBJETOS);
 	sala->interacciones = lista_crear();
 	if (sala->objetos == NULL || sala->interacciones == NULL || 
-	    sala->objetos_conocidos == NULL) {
+	    sala->objetos_conocidos == NULL ||
+	    sala->objetos_poseidos == NULL) {
 		sala_destruir(sala);
 		return NULL;
 	}
@@ -164,6 +202,24 @@ sala_t *sala_crear_desde_archivos(const char *objetos,
 }
 
 
+/*
+ *
+ *
+ */
+bool guardar_nombres(const char *nombre, void *info_objeto, 
+		     void *nombres)
+{
+	struct nombres *nombres_objetos = nombres;
+	char *nombre_objeto = (char *) nombre; // VER POR QUÉ ESTO FUNCIONÓ Y DECIR CÓMO LO ARREGLÉ
+
+	nombres_objetos->vector_nombres[nombres_objetos->cantidad_nombres] = 
+					nombre_objeto; // VER POR QUÉ ESTO FUNCIONÓ Y DECIR CÓMO LO ARREGLÉ
+	(nombres_objetos->cantidad_nombres)++;
+
+	return true;
+}
+
+
 char **sala_obtener_nombre_objetos(sala_t *sala, int *cantidad)
 {
 	if (sala == NULL) {
@@ -172,24 +228,31 @@ char **sala_obtener_nombre_objetos(sala_t *sala, int *cantidad)
 		return NULL;
 	}
 
-	return NULL;
+	int cantidad_objetos = (int) hash_cantidad(sala->objetos);
 
-	char **vector_nombres = malloc((unsigned) lista_tamanio(sala->objetos) * sizeof(char *));
+	char **vector_nombres = malloc((unsigned int) cantidad_objetos * 
+					sizeof(char *));
 	if (vector_nombres == NULL) {
-		free(sala);
+		sala_destruir(sala);
 		if (cantidad != NULL)
 			*cantidad = -1; 
 		return NULL;
 	}
 
-	for (int i = 0; i < hash_cantidad(sala->objetos); i++)
-		vector_nombres[i] = hash_obtener(sala->objetos, )
-		sala->objetos;
-		//sala->objetos[i]->nombre;
+	struct nombres *nombres_objetos = malloc(sizeof(struct nombres));
+	if (nombres_objetos == NULL) {
+		sala_destruir(sala);
+		return NULL;
+	}
+	nombres_objetos->cantidad_nombres = 0;
+	nombres_objetos->vector_nombres = vector_nombres;
+
+	hash_con_cada_clave(sala->objetos, guardar_nombres, nombres_objetos);
 
 	if (cantidad != NULL)
-		*cantidad = hash_cantidad(sala->objetos);
+		*cantidad = cantidad_objetos;
 
+	free(nombres_objetos);
 	return vector_nombres;
 }
 
@@ -288,20 +351,55 @@ int sala_ejecutar_interaccion(sala_t *sala, const char *verbo,
 }
 
 
-bool sala_es_interaccion_valida(sala_t *sala, const char *verbo, const char *objeto1,
-				const char *objeto2)
+/*
+ *
+ *
+ */
+bool verificar_interaccion_valida(void *interaccion_actual, 
+				  void *elementos_interaccion)
 {
-	if (sala == NULL || verbo == NULL || objeto1 == NULL || objeto2 == NULL)
-		return false;
-		
-	for (int i = 0; i < lista_tamanio(sala->interacciones); i++) {
-		if ((strcmp(objeto1, sala->interacciones[i]->objeto) == 0) && 
-		   (strcmp(objeto2, sala->interacciones[i]->objeto_parametro) == 0) && 
-		   (strcmp(verbo, sala->interacciones[i]->verbo) == 0))
-			return true;
-	}
+	struct interaccion *interaccion = interaccion_actual;
+	struct elementos_interaccion *interaccion_a_verificar = 
+						elementos_interaccion;
 
-	return false;
+	if (strcmp(interaccion->objeto, interaccion_a_verificar->objeto1) == 0 &&
+	    (strcmp(interaccion->objeto_parametro, interaccion_a_verificar->objeto2) == 0) &&
+	    (strcmp(interaccion->verbo, interaccion_a_verificar->verbo) == 0)) {
+		interaccion_a_verificar->es_valida = true;
+		return false;
+	    }
+
+	return true;
+}
+
+
+bool sala_es_interaccion_valida(sala_t *sala, const char *verbo, 
+				const char *objeto1, const char *objeto2)
+{
+	if (sala == NULL || verbo == NULL || objeto1 == NULL || 
+	    objeto2 == NULL)
+		return false;
+
+	bool interacccion_valida = false;
+
+	struct elementos_interaccion *elementos_interaccion = 
+				malloc(sizeof(struct elementos_interaccion));
+	if (elementos_interaccion == NULL)
+		return false;
+	elementos_interaccion->objeto1 = objeto1;
+	elementos_interaccion->objeto2 = objeto2;
+	elementos_interaccion->verbo = verbo;
+	elementos_interaccion->es_valida = false;
+
+	lista_con_cada_elemento(sala->interacciones, 
+			verificar_interaccion_valida, elementos_interaccion);
+	
+	if (elementos_interaccion->es_valida)
+		interacccion_valida = true;
+
+	free(elementos_interaccion);
+
+	return interacccion_valida;
 }
 
 
@@ -317,13 +415,37 @@ bool sala_escape_exitoso(sala_t *sala)
 }
 
 
+/*
+ *
+ *
+ */
+/*
+void destruir_objetos(void *info_objeto)
+{
+	struct info_objeto *objeto = info_objeto;
+}
+*/
+
+/*
+ *
+ *
+ */
+void destruir_interacciones(void *interaccion_actual)
+{
+	struct interaccion *interaccion = interaccion_actual;
+	free(interaccion);
+}
+
+
 void sala_destruir(sala_t *sala)
 {
 	if (sala == NULL)
 		return;
 
 	hash_destruir(sala->objetos);
-	lista_destruir(sala->interacciones);
+	hash_destruir(sala->objetos_conocidos);
+	hash_destruir(sala->objetos_poseidos);
+	lista_destruir_todo(sala->interacciones, destruir_interacciones);
 
 	free(sala);
 }
